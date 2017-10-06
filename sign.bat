83be2a91a277b267c653810a288c8791317b8360
Martin Drab <martin.drab@email.cz>
2017-10-06 22:11:39 +0200

    Fixing the sign.bat script

--- sign.bat
+++ sign.bat
@@ -1,5 +1,5 @@
 set SIGNEXE="c:\Program Files (x86)\Windows Kits\10\bin\10.0.15063.0\x64\signtool.exe"
-set FILES=bin\x86\WlanClient.exe bin\x64\WlanClient.exe
+set FILES=bin\Win32\Release\WlanClient.exe bin\x64\Release\WlanClient.exe
 set CROSSCERT="m:\cert\codesigning2017\crosssigning.crt"
 %SIGNEXE% sign /n "Martin Drab" /t http://time.certum.pl /fd sha1 /ac %CROSSCERT% /v %FILES%
 %SIGNEXE% sign /as /n "Martin Drab" /tr http://time.certum.pl /fd sha256 /ac %CROSSCERT% /v %FILES%
