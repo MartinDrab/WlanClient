d7bb9b50118409f298d85e6bce1f4306f07ee83f
Martin Drab <martin.drab@email.cz>
2017-10-07 20:36:07 +0200

    WLAN_HOSTED_NETWORK_REASON

--- src/WLanAPI.pas
+++ src/WLanAPI.pas
@@ -265,6 +265,39 @@ Type
   WLAN_HOSTED_NETWORK_OPCODE = _WLAN_HOSTED_NETWORK_OPCODE;
   PWLAN_HOSTED_NETWORK_OPCODE = ^WLAN_HOSTED_NETWORK_OPCODE;
 
+  _WLAN_HOSTED_NETWORK_REASON = (
+    wlan_hosted_network_reason_success                               = 0,
+    wlan_hosted_network_reason_unspecified,
+    wlan_hosted_network_reason_bad_parameters,
+    wlan_hosted_network_reason_service_shutting_down,
+    wlan_hosted_network_reason_insufficient_resources,
+    wlan_hosted_network_reason_elevation_required,
+    wlan_hosted_network_reason_read_only,
+    wlan_hosted_network_reason_persistence_failed,
+    wlan_hosted_network_reason_crypt_error,
+    wlan_hosted_network_reason_impersonation,
+    wlan_hosted_network_reason_stop_before_start,
+    wlan_hosted_network_reason_interface_available,
+    wlan_hosted_network_reason_interface_unavailable,
+    wlan_hosted_network_reason_miniport_stopped,
+    wlan_hosted_network_reason_miniport_started,
+    wlan_hosted_network_reason_incompatible_connection_started,
+    wlan_hosted_network_reason_incompatible_connection_stopped,
+    wlan_hosted_network_reason_user_action,
+    wlan_hosted_network_reason_client_abort,
+    wlan_hosted_network_reason_ap_start_failed,
+    wlan_hosted_network_reason_peer_arrived,
+    wlan_hosted_network_reason_peer_departed,
+    wlan_hosted_network_reason_peer_timeout,
+    wlan_hosted_network_reason_gp_denied,
+    wlan_hosted_network_reason_service_unavailable,
+    wlan_hosted_network_reason_device_change,
+    wlan_hosted_network_reason_properties_change,
+    wlan_hosted_network_reason_virtual_station_blocking_use,
+    wlan_hosted_network_reason_service_available_on_virtual_station);
+  WLAN_HOSTED_NETWORK_REASON = _WLAN_HOSTED_NETWORK_REASON;
+  PWLAN_HOSTED_NETWORK_REASON = ^WLAN_HOSTED_NETWORK_REASON;
+
 Function WlanOpenHandle(dwClientVersion:DWORD; Reserved:PVOID; Var pdwNegotiatedVersion; Var phClientHandle:THandle):DWORD; StdCall;
 Function WlanCloseHandle(hClientHandle:THandle; Reserved:PVOID):DWORD; StdCall;
 Function WlanEnumInterfaces(hClientHandle:THandle; lpReserved:PVOID; Var pInterfaceList:PWLAN_INTERFACE_INFO_LIST):DWORD; StdCall;
